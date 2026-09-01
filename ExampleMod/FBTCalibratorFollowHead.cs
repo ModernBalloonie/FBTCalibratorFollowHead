7a17323d22075b6690bbd04d5c634aedb405270a
ModernBalloonie <82356528+ModernBalloonie@users.noreply.github.com>
2026-08-31 18:07:49 -0700

    Added forward/back offset and toggle

--- ExampleMod/FBTCalibratorFollowHead.cs
+++ ExampleMod/FBTCalibratorFollowHead.cs
@@ -11,11 +11,18 @@ using ResoniteModLoader;
 namespace FBTCalibratorFollowHead;
 //More info on creating mods can be found https://github.com/resonite-modding-group/ResoniteModLoader/wiki/Creating-Mods
 public class FBTCalibratorFollowHead : ResoniteMod {
-	internal const string VERSION_CONSTANT = "1.0.1"; //Changing the version here updates it in all locations needed
+	internal const string VERSION_CONSTANT = "1.0.2"; //Changing the version here updates it in all locations needed
 	public override string Name => "FBTCalibratorFollowHead";
 	public override string Author => "ModernBalloonie";
 	public override string Version => VERSION_CONSTANT;
 	public override string Link => "https://github.com/ModernBalloonie/FBTCalibratorFollowHead";
+
+	[AutoRegisterConfigKey]
+	private static readonly ModConfigurationKey<bool> MOD_ENABLED = new("modEnabled", "Mod enabled", () => true);
+
+	[Range(-0.25f, 0.25f)]
+	[AutoRegisterConfigKey]
+	private static readonly ModConfigurationKey<float> FORWARD_BACK_OFFSET = new("fbOffset", "Forward Back Offset (Changes the offset of calibration reference)", () => 0f);
 
 	public override void OnEngineInit() {
 		Harmony harmony = new("ModernBalloonie.FBTCalibratorFollowHead");
@@ -26,7 +33,7 @@ public class FBTCalibratorFollowHead : ResoniteMod {
 	class FullBodyCalibrator_OnCommonUpdate_Patch {
 		static void Postfix(FullBodyCalibrator __instance) {
 
-			if (__instance.CalibratingPose == true) {
+			if (__instance.CalibratingPose == true && MOD_ENABLED.Value) {
 
 				// Yeah this is probably jank, but it works.
 
@@ -44,9 +51,7 @@ public class FBTCalibratorFollowHead : ResoniteMod {
 
 				float3 headDir = head.Parent.GlobalDirectionToLocal(head.Forward).x_z.Normalized;
 
-
-
-				vrIK.Slot.LocalPosition = new float3 (head.LocalPosition.X,0f,head.LocalPosition.Z);
+				vrIK.Slot.LocalPosition = new float3 (head.LocalPosition.X,0f,head.LocalPosition.Z) + headDir * FORWARD_BACK_OFFSET.Value; // Offset to move forward and backwards to tweak tracking position
 
 				vrIK.Slot.LocalRotation = floatQ.LookRotation(headDir,new float3(0f,1f,0f));
 
